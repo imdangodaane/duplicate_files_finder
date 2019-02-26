a9784d9d946755057f2d2f53713d291065789d90
imdangodaane <imdangodaane@gmail.com>
2019-02-26 21:59:42 +0700

    Finish Project

--- another.py
+++ another.py
@@ -24,77 +24,113 @@ def chunk_reader(fobj, chunk_size=1024):
 
 
 def get_hash(filename, first_chunk_only=False, hash=hashlib.md5):
+    """Get hash from file"""
     hashobj = hash()
-    with open(filename, 'rb') as file_object:
-        if first_chunk_only:
-            hashobj.update(file_object.read(1024))
-        else:
-            for chunk in chunk_reader(file_object):
-                hashobj.update(chunk)
-        hashed = hashobj.hexdigest()
-    return hashed
+    if os.access(filename, os.R_OK):
+        with open(filename, 'rb') as file_object:
+            if first_chunk_only:
+                hashobj.update(file_object.read(1024))
+            else:
+                for chunk in chunk_reader(file_object):
+                    hashobj.update(chunk)
+            hashed = hashobj.hexdigest()
+        return hashed
 
 
-def group_by_size(file_path_names):
-    hashes_by_size = dict()
-
-    for filename in file_path_names:
-        try:
-            file_size = os.path.getsize(filename)
-        except OSError:
-            continue
-        duplicate = hashes_by_size.get(file_size)
-        if duplicate:
-            hashes_by_size[file_size].append(filename)
-        else:
-            hashes_by_size[file_size] = []
-            hashes_by_size[file_size].append(filename)
-
-    return hashes_by_size
-
-
-def group_on_1k(file_path_names, hash=hashlib.md5):
-    hashes_on_1k = dict()
+def group_by(file_path_names, hash=hashlib.md5, key='size'):
+    """Group files by 3 optional:
+    - By size
+    - By 1024 first bytes
+    - By full file content
+    Choose: key = ['size', '1k', 'content']
+    Return a dictionary contain duplicate files with md5 as key
+    """
+    hashes_by = dict()
 
     for filename in file_path_names:
         try:
-            small_hash = get_hash(filename, first_chunk_only=True)
+            if key == 'size':
+                dict_key = os.path.getsize(filename)
+            elif key == '1k':
+                dict_key = get_hash(filename, first_chunk_only=True)
+            elif key == 'content':
+                dict_key = get_hash(filename, first_chunk_only=False)
         except OSError:
             continue
-        duplicate = hashes_on_1k.get(small_hash)
+        duplicate = hashes_by.get(dict_key)
         if duplicate:
-            hashes_on_1k[small_hash].append(filename)
+            hashes_by[dict_key].append(filename)
         else:
-            hashes_on_1k[small_hash] = []
-            hashes_on_1k[small_hash].append(filename)
-    return hashes_on_1k
+            hashes_by[dict_key] = []
+            hashes_by[dict_key].append(filename)
+
+    return hashes_by
 
 
-def group_by_content(file_path_names, hash=hashlib.md5):
-    hashes_full = dict()
+# def group_by_size(file_path_names):
+#     hashes_by_size = dict()
 
-    for filename in file_path_names:
-        try:
-            full_hash = get_hash(filename, first_chunk_only=False)
-        except OSError:
-            continue
-        duplicate = hashes_full.get(full_hash)
-        if duplicate:
-            hashes_full[full_hash].append(filename)
-        else:
-            hashes_full[full_hash] = []
-            hashes_full[full_hash].append(filename)
+#     for filename in file_path_names:
+#         try:
+#             file_size = os.path.getsize(filename)
+#         except OSError:
+#             continue
+#         duplicate = hashes_by_size.get(file_size)
+#         if duplicate:
+#             hashes_by_size[file_size].append(filename)
+#         else:
+#             hashes_by_size[file_size] = []
+#             hashes_by_size[file_size].append(filename)
 
-    return hashes_full
+#     return hashes_by_size
+
+
+# def group_on_1k(file_path_names, hash=hashlib.md5):
+#     hashes_on_1k = dict()
+
+#     for filename in file_path_names:
+#         try:
+#             small_hash = get_hash(filename, first_chunk_only=True)
+#         except OSError:
+#             continue
+#         duplicate = hashes_on_1k.get(small_hash)
+#         if duplicate:
+#             hashes_on_1k[small_hash].append(filename)
+#         else:
+#             hashes_on_1k[small_hash] = []
+#             hashes_on_1k[small_hash].append(filename)
+#     return hashes_on_1k
+
+
+# def group_by_content(file_path_names, hash=hashlib.md5):
+#     hashes_full = dict()
+
+#     for filename in file_path_names:
+#         try:
+#             full_hash = get_hash(filename, first_chunk_only=False)
+#         except OSError:
+#             continue
+#         duplicate = hashes_full.get(full_hash)
+#         if duplicate:
+#             hashes_full[full_hash].append(filename)
+#         else:
+#             hashes_full[full_hash] = []
+#             hashes_full[full_hash].append(filename)
+
+#     return hashes_full
 
 
 def check_duplicates(file_path_names, hash=hashlib.md5):
+    """
+    Return a list of duplicate files
+    """
     result = []
     hashes_by_size = dict()
     hashes_on_1k = dict()
     hashes_full = dict()
 
-    hashes_by_size = group_by_size(file_path_names)
+    # hashes_by_size = group_by_size(file_path_names)
+    hashes_by_size = group_by(file_path_names, key='size')
 
     logging.debug("Time: " + str(datetime.now()))
     logging.debug("List hashes by size:")
@@ -104,7 +140,8 @@ def check_duplicates(file_path_names, hash=hashlib.md5):
         if len(values) < 2:
             continue
         else:
-            hashes_on_1k.update(group_on_1k(values))
+            # hashes_on_1k.update(group_on_1k(values))
+            hashes_on_1k.update(group_by(values, key='1k'))
 
     logging.debug("List hashes on 1k:")
     logging.debug(hashes_on_1k)
@@ -113,7 +150,8 @@ def check_duplicates(file_path_names, hash=hashlib.md5):
         if len(values) < 2:
             continue
         else:
-            hashes_full.update(group_by_content(values))
+            # hashes_full.update(group_by_content(values))
+            hashes_full.update(group_by(values, key='content'))
 
     logging.debug("List hashes by full content:")
     logging.debug(hashes_full)
@@ -126,6 +164,7 @@ def check_duplicates(file_path_names, hash=hashlib.md5):
 
     logging.debug("Result list:")
     logging.debug(result)
-    logging.debug('<===============================END-OF-MESSAGE===============================>\n')
+    logging.debug('<===============================END-OF-MESSAGE\
+                  ===============================>\n')
 
     return result
--- find_duplicate_files.py
+++ find_duplicate_files.py
@@ -9,19 +9,30 @@ import another
 
 
 def get_arguments():
+    """Get argument from command-line interface
+    Choose path with -p/--path option
+    Choose show hidden or not with -i/--hidden option
+    """
     parser = argparse.ArgumentParser(description='Ahahehihohu')
-    parser.add_argument('-p', '--path', type=str, 
-                        help='Root directory to start scanning for duplicate files')
+    parser.add_argument('-p', '--path', type=str,
+                        help='Root directory to start scanning \
+                              for duplicate files')
     parser.add_argument('-i', '--hidden', action='store_true',
                         help='Show hidden files')
     return parser.parse_args()
 
 
 def path_valid(path):
+    """
+    Return True if exist path
+    Exit program when path does not exist
+    Write to error.log
+    """
     if os.path.exists(path):
         return True
     else:
-        error_log = ": ".join([str(os.path.basename(sys.argv[0])), "path_valid", path, "Invalid path.\n"])
+        error_log = ": ".join([str(os.path.basename(sys.argv[0])),
+                              "path_valid", path, "Invalid path.\n"])
         with open('error.log', 'a+') as fd:
             fd.write("Time: " + str(datetime.datetime.now()) + "\n")
             fd.write(error_log + "\n\n")
@@ -29,17 +40,26 @@ def path_valid(path):
         sys.exit(1)
 
 
+def file_valid(filename):
+    """Check for readable file"""
+    return os.access(filename, os.R_OK)
+
+
 def scan_files(path, show_hidden):
+    """Return list of files of path"""
     if path_valid(path):
         list_of_files = list()
         for (dir_path, dir_names, file_names) in os.walk(path):
+            if not show_hidden:
+                file_names = [f for f in file_names if not f[0] == '.']
+                dir_names[:] = [d for d in dir_names if not d[0] == '.']
             try:
-                if not show_hidden:
-                    file_names = [f for f in file_names if not f[0] == '.']
-                    dir_names[:] = [d for d in dir_names if not d[0] == '.']
                 for file_name in file_names:
-                    if not os.path.islink(os.path.join(dir_path, file_name)):
-                        full_path = os.path.realpath(os.path.join(dir_path, file_name))
+                    if (file_valid(os.path.join(dir_path, file_name))
+                       and not os.path.islink(os.path.join(dir_path,
+                                                           file_name))):
+                        full_path = os.path.realpath(os.path.join(dir_path,
+                                                                  file_name))
                         list_of_files += [full_path]
             except OSError:
                 continue
@@ -47,6 +67,7 @@ def scan_files(path, show_hidden):
 
 
 def group_files_by_size(file_path_names):
+    """Simple function group file by size"""
     result_group = list()
     group_by_size = dict()
     for _file in file_path_names:
@@ -62,23 +83,22 @@ def group_files_by_size(file_path_names):
     for keys, values in group_by_size.items():
         if len(values) > 1:
             result_group.append(list(value for value in values))
-        # temp = list()
-        # for value in values:
-        #     temp.append(value)
-        # result_group.append(temp)
     return result_group
 
 
 def get_file_checksum(file_path_name):
+    """Simple function get file checksum"""
     if path_valid(file_path_name):
-        with open(file_path_name, "rb") as fd:
-            content = fd.read()
-            h = hashlib.md5()
-            h.update(content)
-        return h.hexdigest()
+        if file_valid(file_path_name):
+            with open(file_path_name, "rb") as fd:
+                content = fd.read()
+                h = hashlib.md5()
+                h.update(content)
+            return h.hexdigest()
 
 
 def group_files_by_checksum(file_path_names):
+    """Simple function group files by checksum"""
     result_group = list()
     group_by_checksum = dict()
     for _file in file_path_names:
@@ -99,6 +119,11 @@ def group_files_by_checksum(file_path_names):
 
 
 def find_duplicate_files(file_path_names):
+    """
+    Simple function find duplicate files
+    Group files by size first and then
+    group files by checksum using group_by function above
+    """
     tmp = list()
     group_by_size = group_files_by_size(file_path_names)
     if len(group_by_size) > 0:
@@ -110,24 +135,23 @@ def find_duplicate_files(file_path_names):
 
 
 def json_dump(data):
+    """
+    Return JSON formatted string
+    """
     return json.dumps(data)
 
 
 def main():
+    """Main function operate program"""
     args = get_arguments()
     path = args.path
     show_hidden = args.hidden
     list_of_files = scan_files(path, show_hidden)
-    # print(list_of_files)
     # group_by_size = group_files_by_size(list_of_files)
     # group_by_checksum = group_files_by_checksum(list_of_files)
     # print(group_by_size)
     # print(group_by_checksum)
     # result = find_duplicate_files(list_of_files)
-    # print(result)
-    # print(type(result))
-    # print(json_dump(result))
-    # print(type(json_dump(result)))
     result = another.check_duplicates(list_of_files)
     print(json_dump(result))
 
